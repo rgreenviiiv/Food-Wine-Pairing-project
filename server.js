fc4920d2cbf916dafc9627d933f804e8e10ee0ec
Riley Jamel Green <rileygreenviiiv@gmail.com>
2018-03-17 14:35:01 -0400

    update PORT
    updating the port to process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const axios = require('axios');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const request = require('request-promise');
 
 function queryFood2Fork(item) {
